f6de555d3a623d30716f2e9da3273be6ead5fc3b
Arturo Suárez <manuel.suarez@cimat.mx>
2022-09-01 14:13:53 -0500

    Apply underwater effect

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
+import numpy as np
 from PIL import Image
 
-root_folder = r"C:\Users\masua\Downloads\Cimat\Nyu v2\NYU_GT"
-base_img = "NYU_GT"
-clean_img = "NYU_CL"
-under_img = "NYU_UW"
+root_folder = r"C:\Users\masua\Downloads\Cimat\Nyu v2"
+base_dir = "NYU_GT"
+clean_dir = "NYU_CL"
+under_dir = "NYU_UW"
 
 # Type of submarine ambient
 type_I = [0.85, 0.961, 0.982]
@@ -18,14 +19,53 @@ type_5 = [0.67, 0.73, 0.67]
 type_7 = [0.62, 0.61, 0.5]
 type_9 = [0.55, 0.46, 0.29]
 
+TYPE = type_1
+
 # Traverse all files in directory
-for num_image in range(1, 1450):
+for num_image in range(1, 2):
     # Conform image and depth filenames
     fname = f"{num_image}_Image_.bmp"
     dname = f"{num_image}_Depth_.bmp"
     # Open images
-    image = Image.open(os.path.join(root_folder, base_img, fname))
-    depth = Image.open(os.path.join(root_folder, base_img, dname))
+    image = np.array(Image.open(os.path.join(root_folder, base_dir, fname)))
+    print("Image: ", image.shape, image.dtype, np.max(image), np.min(image))
+    depth = np.array(Image.open(os.path.join(root_folder, base_dir, dname)))
+    print("Depth: ", depth.shape, depth.dtype, np.max(depth), np.min(depth))
     # Remove blank edges
-    image0 = image[0:-11, 0:-11, :]
-    depth0 = depth[0:-11, 0:-11, :] / 255 # Depth between 0 and 1
+    image0 = image[10:-10, 10:-10, :] / 255
+    depth0 = depth[10:-10, 10:-10] / 255
+
+    print("Image0: ", image0.shape, image0.dtype, np.max(image0), np.min(image0))
+    print("Depth0: ", depth0.shape, depth0.dtype, np.max(depth0), np.min(depth0))
+
+    # Parámetros aleatorios de profundidad y horizonte
+    deep = 5 - 2 * np.random.rand()
+    horization = 15 - 14.5 * np.random.rand()
+
+    # Término beta
+    A1 = 1.5 * TYPE[0]**deep
+    A2 = 1.5 * TYPE[1]**deep
+    A3 = 1.5 * TYPE[2]**deep
+    # Término t
+    t1 = TYPE[0]**(depth0 * horization)
+    t2 = TYPE[1]**(depth0 * horization)
+    t3 = TYPE[2]**(depth0 * horization)
+
+    print("T1", t1.shape, t1.dtype)
+    print("T2", t2.shape, t2.dtype)
+    print("T3", t3.shape, t3.dtype)
+
+    # Underwater image
+    I = np.zeros(image0.shape)
+    print("I", I.shape, I.dtype)
+    I[:, :, 0] = image0[:, :, 0] * t1 + (1 - t1) * A1
+    I[:, :, 1] = image0[:, :, 1] * t2 + (1 - t2) * A2
+    I[:, :, 2] = image0[:, :, 2] * t3 + (1 - t3) * A3
+    print("I", I.shape, I.dtype)
+
+    # Guardamos imagen
+    clean_img = Image.fromarray((image0 * 255).astype(np.uint8))
+    clean_img.save(os.path.join(root_folder, under_dir, fname))
+    underwater_img = Image.fromarray((I * 255).astype(np.uint8))
+    uname = f"{num_image}_Underwater_.bmp"
+    underwater_img.save(os.path.join(root_folder, under_dir, uname))
